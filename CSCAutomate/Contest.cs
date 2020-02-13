79a923260ded551924412080e2b2516178caaf1c
Frank Sposaro <frank.sposaro@gmail.com>
2020-02-12 23:59:33 -0800

    Initial Commit

---
+++ CSCAutomate/Contest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+using System.Text;
+
+namespace CSCAutomate
+{
+    class Contest
+    {
+        public string name { get; set; }
+        public string challengeDescription { get; set; }
+        public string hasPrizes { get; set; }
+        public string type { get; set; }
+        public string StartDateStr { get; set; }
+        public string EndDateStr { get; set; }
+        public string customCss { get; set; }
+        public string templateSelection { get; set; }
+        public string selfRegistrationEnabled { get; set; }
+        public string createdBy { get; set; }
+        public string microsoftAccountSponsor { get; set; }
+        public string country { get; set; }
+        public string mstpid { get; set; }
+        public string eou { get; set; }
+        public string accountType { get; set; }
+        public string timeZone { get; set; }
+        public string allowTeams { get; set; }
+        public string teams { get; set; }
+        public string mpnid { get; set; }
+        public string participantType { get; set; }
+        public string collectionID { get; set; }
+        public string collectionName { get; set; }
+        public string collectionUrl { get; set; }
+    }
+}
---
+++ CSCAutomate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CSCAutomate
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task Main(string[] args)
+        {
+            string response = await CreateChallengeAsync(ContestTests.TestContest);
+
+            Console.Out.WriteLine(response);
+
+            Console.In.Read();
+        }
+
+        public static async Task<string> CreateChallengeAsync(Contest contest)
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            string API_KEY = "<Omitted>";
+            string uri = "<Omitted>" + queryString;
+            HttpResponseMessage response;
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", API_KEY);
+
+            // Request body
+            string json = JsonConvert.SerializeObject(contest);
+
+            // Make Call
+            var responseData = await client.PostAsync(
+                                        uri,
+                                        new StringContent(json, Encoding.UTF8, "application/json"));
+
+            return "called";
+        }
+    }
+}
