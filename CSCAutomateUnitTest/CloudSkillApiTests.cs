b26076b7aed25b8d82eb1d74a25524ac052a7c80
sriniambati <59178896+sriniambati@users.noreply.github.com>
2020-03-26 00:06:02 -0400

    Added AddLearnerToChallengeFromHttp function (#6)
    * Fixed Unit Tests
    
    * Added AddLearnerToChallengeFromHttp function
    
    Co-authored-by: Srini Ambati <ambatis@gmail.com>

--- CSCAutomateUnitTest/CloudSkillApiTests.cs
+++ CSCAutomateUnitTest/CloudSkillApiTests.cs
@@ -1,4 +1,5 @@
-﻿using CSCAutomateUnitTest;
+﻿using CSCAutomateLib;
+using CSCAutomateUnitTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -17,10 +18,22 @@ namespace CSCAutomateLib.Tests
             CloudSkillApi cscApi = new CloudSkillApi(config);
 
             string challengeRequestJson = ContestFactory.CreateChallengeRequestJson();
-            List<ContestResponse> contestReponseList = await cscApi.CreateChallengesAsyc(blobApi, challengeRequestJson);
+            List<ContestResponse> contestReponseList = await cscApi.CreateChallengesAsync(blobApi, challengeRequestJson);
 
             Assert.IsTrue(contestReponseList.Count > 0);
-        }        
+        }
+
+        [TestMethod()]
+        public async Task AddLearnerAsyncTest()
+        {
+            Configuration config = await TestHelper.GetConfigurationAsync();
+            CloudSkillApi cscApi = new CloudSkillApi(config);
+
+            string learnerRequestJson = ContestFactory.CreateLearnerRequestJson();
+            string learnerReponse = await cscApi.AddLearnerAsync(learnerRequestJson);
+
+            Assert.IsTrue(learnerReponse.Length > 0);
+        }
         #endregion
     }
 }
